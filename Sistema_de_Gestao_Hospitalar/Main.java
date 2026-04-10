d25ad513480d5ffdf39b948515ce9e3688f7ac8e
Keyze Torres <jsantos.torres2006@gmail.com>
2026-04-10 01:35:07 +0000

    CADA VEZ MAIS TRISTE.

--- Sistema_de_Gestao_Hospitalar/Main.java
+++ Sistema_de_Gestao_Hospitalar/Main.java
@@ -1,39 +1,56 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int op;
+        int qtdPacientes = 0;
+        int qtdMedicos = 0;
 
-        Menu m = new Menu();
-        m.menu();
+        do {
+            Menu m = new Menu();
+            m.menu();
 
-        //int op;
-        System.out.println("Informe a opção desejada:");
-        int op = sc.nextInt();
+            System.out.println("Informe a opção desejada:");
+            op = sc.nextInt();
 
-        switch(op) {
-            case 1:
-                Paciente p[] = {
-                new Paciente("", 0, "", "", "");
-                p.setNome();
-                p.setIdade();
-                p.setCPF();
-                p.setSusCard();
-                p.setHistorico();
-                };
-                break;
+            switch(op) {
+                case 1:
+                    ArrayList<Paciente> pacientes = new ArrayList<>();
+                    for (int i = 0; i < 10; i++){
+                        if(pacientes.get(i).getNome() == null){
+                            pacientes.add(new Paciente("", 0, "", "", ""));
+                            pacientes.get(i).setNome();
+                            pacientes.get(i).setIdade();
+                            pacientes.get(i).setCPF();
+                            pacientes.get(i).setSusCard();
+                            pacientes.get(i).setHistorico();
 
-            case 2:
-                Medico m[] = {
-                    new Medico("", 0, "", "", 0);
-                    m.setNome();
-                    m.setIdade();
-                    m.setCPF();
-                    m.setEspecialidade();
-                    m.setSalario();
-                };
-                break;
-        }
+                            qtdPacientes++;
+                            break;
+                        }
+                    }
+                    break;
+
+                case 2:
+                    ArrayList<Medico> medicos = new ArrayList<>();
+                    for (int i = 0; i < 10; i++){
+                        if(medicos.get(i).getNome() == null){
+                            medicos.add(new Medico("", 0, "", "", 0                            ));
+                            medicos.get(i).setNome();
+                            medicos.get(i).setIdade();
+                            medicos.get(i).setCPF();
+                            medicos.get(i).setEspecialidade();
+
+                            qtdMedicos++;
+                            break;
+                        }
+                    }
+                    break;
+            }
+
+        } while (op != 0);
     }
 }
